1d1605a297de7edf6619d517225367c5a77eb1b6
DeveloperAnonymous <matthew_quebec@outlook.fr>
2019-10-02 22:04:29 -0400

    initial release

---
+++ js/shop.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $.ajax({
+        type: "get",
+        url: "data.json",
+        beforeSend: function () {
+            $("#items").html("Chargement...");
+        },
+        timeout: 10000,
+        error: function (xhr, status, error) {
+            $("#items").html("Une erreur est survenue lors du chargement du magasin!");
+        },
+        dataType: "json",
+        success: function (data) {
+            let balise = $("#items");
+            balise.html("");
+            $.each(data["shop"], function (key, value) {
+                balise.append(
+                    "<td><div class=\"shop-item\">" +
+                    "<img alt='" + value.description + "' src=\"assets/shop/" + value.photo + "\"> " +
+                    "<form action='#'>" +
+                    "<label><strong>Nom:</strong> " + value.nom + "</label>" +
+                    "<label><strong>Prix:</strong> " + value.prix + "</label>" +
+                    "<label for='qty'><strong>Quantité</strong> <input required type=\"number\" min='1' max='10' placeholder=\"0\" name='qty'></label>" +
+                    "<button class='btn btn-dark' type='submit'>Acheter!</button></form>" +
+                    "</div></td>"
+                );
+            });
+
+            // Affiche un rabais s'il y en a un d'actif
+            let rabais = $("#rabais");
+            $.each(data["promotions"], function(key, value) {
+                if (value["active"]) {
+                    let date = new Date(value["endDate"]).getTime();
+                    var x = setInterval(function() {
+                        let today = new Date().getTime();
+                        let diff = date - today;
+                        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                        let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                        let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+                        let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+                        let formatedDate = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+                        rabais.html("<code class=\"date\">" + formatedDate + "</code> " + value["desc"] + " <code>" + value["code"] + "</code>");
+                        rabais.css("display", "block");
+                        if (diff < 0) {
+                            clearInterval(x);
+                            rabais.css("display", "none");
+                            rabais.html("EXPIRÉ");
+                        }
+                    }, 1000);
+                }
+            });
+        }
+    });
+});
