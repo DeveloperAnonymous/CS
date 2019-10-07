1e184af018e6c495d29126d7342ec25f33377aa6
Duteau Matthew <1774134@cegepmontpetit.ca>
2019-10-07 15:04:33 -0400

    Updates

--- js/shop.js
+++ js/shop.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-    $(".img-shop").magnificPopup({type:'image'})
-        .magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        mainClass: 'mfp-img-mobile',
-        image: {
-            verticalFit: true
-        },
-        midClick: true
-    });
-
     $.ajax({
         type: "get",
         url: "data.json",
